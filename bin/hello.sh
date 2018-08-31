616b9c89c9be26133207843799c0b4ab23054322
梁海阳 <lianghaiyang@bonc.com.cn>
2018-08-31 17:35:00 +0800

    更新脚本

--- bin/hello.sh
+++ bin/hello.sh
@@ -3,4 +3,4 @@
 cd `dirname $0`
 #export JAVA_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=8089"
 
-nohup java $JAVA_OPTS -jar ./kafka-sender.jar.jar --spring.config.location=../config/application.yml >nohup.txt 2>&1 &
+nohup java $JAVA_OPTS -jar ./kafka-sender.jar --spring.config.location=../config/application.yml >nohup.txt 2>&1 &
